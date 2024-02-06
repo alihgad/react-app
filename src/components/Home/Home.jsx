858fd6654339d4d7c23bbb0df28dddcb197a4da5
ali <alihgad2@gmail.com>
2024-02-06 18:17:40 +0200

    transition

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ali from '../../assists/images/ali.png'
 import LineThrougStar from '../Line_Throug_star/Line_Throug_star';
+import styles from './Home.module.css';
 
 
 export default function Home() {
@@ -9,11 +10,8 @@ export default function Home() {
         <section className=' bg-main  '>
           <div className="container d-flex justify-content-center align-items-center">
             <div className="row row-cols-1">
-            {/* <svg className=' ' height={"300px"}>
-            <ReactLogo   />
-            </svg> */}
-            <div className="photo w-25 mx-auto mt-4 rounded-circle rounded">
-              <img src={ali} alt="ali" className='w-100 rounded-circle border' />
+            <div className="photo w-25 mx-auto mt-5 rounded-circle rounded">
+              <img src={ali} alt="ali" className={`${styles.ali} w-100 rounded-circle border `} />
             </div>
             <h1 className='text-center mt-3 text-white text-uppercase'>start framework</h1>
             <LineThrougStar/>
